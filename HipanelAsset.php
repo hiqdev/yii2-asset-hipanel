a73cc6ab3d242bed86f15c3afe22f3fa11a53742
Andrii Vasyliev <sol@hiqdev.com>
2015-06-11 12:47:25 +0000

    + the asset class file

---
+++ HipanelAsset.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace hiqdev\assets\hipanel;
+
+class HipanelAsset extends \yii\web\AssetBundle
+{
+    /**
+     * @inheritdoc
+     */
+    public $sourcePath = '@hiqdev/assets/hipanel/assets';
+}
