aac4c8a3f396d6cc276cd4a9d5e2a18377a7cba6
Andrii Vasyliev <sol@hiqdev.com>
2015-08-21 14:33:34 +0000

    php-cs-fixed

--- src/HipanelAsset.php
+++ src/HipanelAsset.php
@@ -1,11 +1,20 @@
 <?php
 
+/*
+ * Yii2 asset for HiPanel logo and so on
+ *
+ * @link      https://github.com/hiqdev/yii2-asset-hipanel
+ * @package   yii2-asset-hipanel
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015, HiQDev (https://hiqdev.com/)
+ */
+
 namespace hiqdev\assets\hipanel;
 
 class HipanelAsset extends \yii\web\AssetBundle
 {
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public $sourcePath = '@hiqdev/assets/hipanel/assets';
 }
